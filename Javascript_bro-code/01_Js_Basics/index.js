58f509e3b5b7804c2ad925cb89d210cda92be9fe
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-03 12:48:09 +0530

    Basics of Javascript

---
+++ Javascript_bro-code/01_Js_Basics/index.js
@@ -0,0 +1,31 @@
+// Handles interactivity of the webpage
+
+
+//1--console.log
+
+//console.log is used to print something at console(a dev tool).. not directly at the webpage
+console.log(`Hello`);
+console.log(`Name: Aryan`);
+
+//2--Comments
+
+//This is a single-line comment in Js
+
+/*
+This is
+a
+multi-line comment in Js
+*/
+
+//3--Alerts
+
+window.alert("Hello");
+window.alert("This is a window alert created using Js");
+
+//4--Get Element by ID + Text-Content(Printing content directly on webpage using Js)
+
+document.getElementById("heading").textContent = "Details:";
+document.getElementById("paragraph-2").textContent = "Name - Aryan";
+
+
+
