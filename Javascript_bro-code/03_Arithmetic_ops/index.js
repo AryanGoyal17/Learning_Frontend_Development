045964aa530515ba886a5b3cd56aa056d6fd4a07
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-03 12:49:38 +0530

    Arithmetic Ops and Operator precedence in JavaScript

---
+++ Javascript_bro-code/03_Arithmetic_ops/index.js
@@ -0,0 +1,13 @@
+let students = 30;
+
+students = students + 3;
+console.log(students);
+
+students /= 11;
+console.log(students);
+
+students--;
+console.log(students);
+
+let result = 12 % 5 + 8 / 2;
+console.log(result)
