f3dcf09c40225a38eb3da51ed0df792256ffc576
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-07 12:07:31 +0530

    Type Conversion In Js

---
+++ Javascript_bro-code/05_Type_Conversion/index.js
@@ -0,0 +1,77 @@
+//1
+
+let age1 = window.prompt("Enter your age1: ")
+
+age1 += 1
+
+console.log(age1, typeof age1)
+
+//2
+
+let age2 = window.prompt("Enter your age2: ")
+
+age2++
+
+console.log(age2, typeof age2)
+
+//3
+
+let x = "pizza"
+let y = "pizza"
+let z = "pizza"
+
+x = Number(x)
+console.log(x, typeof x)
+
+y = String(y)
+console.log(y, typeof y)
+
+z = Boolean(z)
+console.log(z, typeof z)
+
+//4 
+
+let x2 = "0"
+let y2 = "0"
+let z2 = "0"
+
+x2 = Number(x2)
+console.log(x2, typeof x2)
+
+y2 = String(y2)
+console.log(y2, typeof y2)
+
+z2 = Boolean(z2)
+console.log(z2, typeof z2)
+
+//5
+
+let x3 = ""
+let y3 = ""
+let z3 = ""
+
+x3 = Number(x3)
+console.log(x3, typeof x3)
+
+y3 = String(y3)
+console.log(y3, typeof y3)
+
+z3 = Boolean(z3)
+console.log(z3, typeof z3)
+
+//6
+
+let x4;
+let y4;
+let z4;
+
+x4 = Number(x4)
+console.log(x4, typeof x4)
+
+y4 = String(y4)
+console.log(y4, typeof y4)
+
+z4 = Boolean(z4)
+console.log(z4, typeof z4)
+
+
