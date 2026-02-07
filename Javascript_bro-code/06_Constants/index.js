67cf5a6cbd9ee168453169b77970612af7a44bd5
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-07 12:26:41 +0530

    Added Type-Casting

--- Javascript_bro-code/06_Constants/index.js
+++ Javascript_bro-code/06_Constants/index.js
@@ -6,6 +6,7 @@ let radius;
 let Circumference;
 
 radius = window.prompt(`Enter the radius of the circle: `)
+radius = Number(radius) //The code works correctly even without Type-casting, because since a string cant be multiplied, Js converts it to a number automatically
 Circumference = 2 * PI * radius
 
 console.log(`The Circumference of the Circle is ${Circumference}`)
