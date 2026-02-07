651947fc24ecfeb9c4e6b4a6f35d6737abe5b62d
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-07 15:15:39 +0530

    Math Objects in Js

---
+++ Javascript_bro-code/07_Math_Objects/index.js
@@ -0,0 +1,37 @@
+let x = 10
+let y = -1.2
+let z = 0
+let m = 81
+let n = 2
+
+//Printing constants using Math Object
+
+console.log(Math.PI)
+console.log(Math.E)
+
+//Using mathematical functions provided by Math object
+
+console.log(Math.round(y))
+console.log(Math.floor(y))
+console.log(Math.ceil(y))
+console.log(Math.trunc(y))
+
+console.log(Math.pow(x, z))
+
+console.log(Math.sqrt(m))
+console.log(Math.sqrt(n))
+
+console.log(Math.log(n)) //Natural log (ln) is taken by Math.log
+
+console.log(Math.sin(z))
+console.log(Math.cos(z))
+console.log(Math.tan(z))
+
+console.log(Math.abs(y))
+
+console.log(Math.sign(x))
+console.log(Math.sign(y))
+console.log(Math.sign(z))
+
+console.log(Math.max(x, y, z))
+console.log(Math.min(x, y, z))
