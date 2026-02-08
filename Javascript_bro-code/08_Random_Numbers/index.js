3b314b8c20dadc2a2611377b8d58fba342e49080
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-08 17:32:08 +0530

    Random Numbers in Js

---
+++ Javascript_bro-code/08_Random_Numbers/index.js
@@ -0,0 +1,6 @@
+const min = 50;
+const max = 100;
+
+const randomNum = Math.floor(Math.random() * (max - min + 1) + min); //Any of const or let can be used for variable -- randomNum
+
+console.log(randomNum);
