0b2548aabe17a6a6990c72c13f76ae6c953adf32
AryanGoyal17 <aryan.goyaltiet@gmail.com>
2026-02-09 18:33:00 +0530

    Code Improved_Ternary op and Switches in Js

--- Javascript_bro-code/11_ternary_operator_AND_switches/index.js
+++ Javascript_bro-code/11_ternary_operator_AND_switches/index.js
@@ -10,28 +10,30 @@ console.log(`The amount of money you have to pay is $${purchaseAmount - ((purcha
 
 //Switches program(print grade of student after comparing with marks)::
 
-let marks = 100;
+let marks = 10;
 let grade;
 
 switch(true){
 
     case marks >= 90 && marks <=100:
         grade = 'A';
-        console.log(grade)
+        // console.log(grade)
         break;
 
     case marks >= 70 && marks < 90:
         grade = 'B';
-        console.log(grade)
+        // console.log(grade)
         break;
 
     case marks >= 50 && marks < 70:
         grade = 'C';
-        console.log(grade)
+        // console.log(grade)
         break;
 
     default:
         grade = 'F';
-        console.log(grade)
+        // console.log(grade)
         break;
 }
+
+console.log(grade);
